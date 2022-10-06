9511476ec6bd214ac1d90132500d70d14b1ee3ad
Anthony Nguyen <48093087+Anthony-D11@users.noreply.github.com>
2022-10-06 02:03:31 -0300

    Created playlist page

---
+++ app/src/main/java/ca/unb/mobiledev/myapplication/PlaylistActivity.kt
@@ -0,0 +1,13 @@
+package ca.unb.mobiledev.karaokeapp.ui
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PlaylistActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
