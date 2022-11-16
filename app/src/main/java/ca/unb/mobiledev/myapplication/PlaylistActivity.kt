020046b8efa94aa64fce6813aeaa8dd7f79dca08
Anthony Nguyen <48093087+Anthony-D11@users.noreply.github.com>
2022-11-16 17:13:43 -0400

    Modifying avatar and background photo feature added

--- app/src/main/java/ca/unb/mobiledev/myapplication/PlaylistActivity.kt
+++ app/src/main/java/ca/unb/mobiledev/myapplication/PlaylistActivity.kt
@@ -3,16 +3,22 @@ package ca.unb.mobiledev.myapplication
 import android.content.Context
 import android.content.ContextWrapper
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.media.Image
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.Environment
+import android.os.storage.StorageManager
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -23,11 +29,17 @@ class PlaylistActivity: AppCompatActivity() {
     lateinit var addSongsButton: FloatingActionButton
     lateinit var playButton: FloatingActionButton
     lateinit var nextButton: FloatingActionButton
+    lateinit var avatar_edit_button: ImageButton
+    lateinit var background_edit_button: ImageButton
     lateinit var playlistAvatar: ImageView
     lateinit var playlistBackground: ImageView
     lateinit var songName_bb: TextView
     lateinit var authorName_bb: TextView
     lateinit var songAvatar_bb: ImageView
+
+    private var filePicker: ActivityResultLauncher<Intent>? = null
+    private var currentModify = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.playlist)
@@ -39,6 +51,17 @@ class PlaylistActivity: AppCompatActivity() {
         songName_bb = findViewById(R.id.songName_bb)
         authorName_bb = findViewById(R.id.authorName_bb)
         songAvatar_bb = findViewById(R.id.songAvatar_bb)
+
+        avatar_edit_button = findViewById(R.id.avatar_edit_button)
+        background_edit_button = findViewById(R.id.background_edit_button)
+
+
+        avatar_edit_button.setOnClickListener {
+            editAvatar()
+        }
+        background_edit_button.setOnClickListener {
+            editBackground()
+        }
 
         var intent: Intent = getIntent()
         var playlistId: String? = intent.getStringExtra(Intent.EXTRA_INDEX)
@@ -54,6 +77,42 @@ class PlaylistActivity: AppCompatActivity() {
         var recyclerView = findViewById<RecyclerView>(R.id.songList)
         var adapter = SongAdapter(songList, mediaPlayer, this@PlaylistActivity)
         recyclerView.adapter = adapter
+
+        setupFilePicker()
+    }
+
+    private fun editAvatar() {
+        currentModify = "avatar"
+        openFilePicker()
+    }
+
+    private fun editBackground() {
+        currentModify = "background"
+        openFilePicker()
+    }
+
+    private fun openFilePicker() {
+        var intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.setType("image/*")
+        intent = Intent.createChooser(intent, "Choose a image file")
+        filePicker!!.launch(intent)
+
+    }
+    private fun setupFilePicker() {
+        filePicker = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            result: ActivityResult ->
+            if (result.resultCode == RESULT_OK) {
+                val data = result.data
+                val uri = data!!.data
+                if (currentModify == "avatar") {
+                    Log.i("Hello", "Hello")
+                    playlistAvatar.setImageURI(uri)
+                }
+                if (currentModify == "background") {
+                    playlistBackground.setImageURI(uri)
+                }
+            }
+        }
     }
     class SongAdapter(private val songList: ArrayList<Song>, private val mediaPlayer: MediaPlayer, private val parentActivity: PlaylistActivity)
         :RecyclerView.Adapter<SongAdapter.ViewHolder>(){
